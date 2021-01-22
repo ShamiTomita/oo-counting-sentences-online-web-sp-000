b3e41c14177938678039900600050bd07c4b1d49
Shami Tomita <shamitomita@gmail.com>
2021-01-22 18:58:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     sentence_count = 0 
-    array = self.split("")
+    array = self.split(/[.?!]/)
     senetence_count = array.count("..."||".") + array.count("??"||"?") + array.count("!!"||"!")
     #binding.pry
     return senetence_count
